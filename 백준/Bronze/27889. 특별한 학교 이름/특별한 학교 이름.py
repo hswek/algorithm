ab44e5d0a413eddeca75c09b81c7c9e17ac86b20
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:25:24 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 148 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+a=input()
+if a=='NLCS':
+    print('North London Collegiate School')
+elif a=='BHA':
+    print('Branksome Hall Asia')
+elif a=='KIS':
+    print('Korea International School')
+elif a=='SJA':
+    print('St. Johnsbury Academy')
