9d399b12f8d8768921faacad91270d19b7f5ffda
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:29:57 +0900

    [Bronze V] Title: 콜센터, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+import heapq
+#a,b=map(int,sys.stdin.readline().rstrip().split())
+print('     /~\\')
+print('    ( oo|')
+print('    _\=/_')
+print('   /  _  \\')
+print('  //|/.\\|\\\\')
+print(' ||  \\ /  ||')
+print('============')
+print('|          |')
+print('|          |')
+print('|          |')
