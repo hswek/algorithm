ee0406c92d6f03c0988e91d20057d2ed9fdeab89
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:08:13 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+n,m=map(int,input().split())
+def a(arr,n,m,idx):
+    if m==0:
+        for i in range(len(arr)):
+            
+            print(arr[i],end=' ')
+        print()
+        return
+    for i in range(idx,n+1):
+        arr.append(i)
+        a(arr,n,m-1,i+1)
+        arr.pop()
+    
+arr=[]
+a(arr,n,m,1)
