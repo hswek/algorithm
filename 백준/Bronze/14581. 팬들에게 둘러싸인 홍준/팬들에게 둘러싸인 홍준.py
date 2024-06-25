910add1102c01192f64849cc284463fa4e94a1fa
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:05:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 172 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.py
@@ -0,0 +1,8 @@
+import sys
+from collections import deque
+import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+n=input()
+print(':fan::fan::fan:')
+print(':fan::'+n+'::fan:')
+print(':fan::fan::fan:')
