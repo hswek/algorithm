f824777f3f0590ad3889d2ec8c33a043149c2e64
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:41:02 +0900

    [Bronze IV] Title: 평균 점수, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,11 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+a=max(int(input()),40)
+b=max(int(input()),40)
+c=max(int(input()),40)
+d=max(int(input()),40)
+e=max(int(input()),40)
+print(sum([a,b,c,d,e])//5)
