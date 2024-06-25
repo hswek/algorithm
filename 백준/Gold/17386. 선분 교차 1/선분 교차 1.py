43f56eebdb2f96176d41acdea94ac94dd982d6a2
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:14:02 +0900

    [Gold III] Title: 선분 교차 1, Time: 152 ms, Memory: 242440 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+import heapq
+from itertools import *
+import math
+sys.setrecursionlimit(10**5)
+x1,y1,x2,y2=list(map(int,sys.stdin.readline().rstrip().split()))
+x3,y3,x4,y4=list(map(int,sys.stdin.readline().rstrip().split()))
+
+def check_line(x1,y1,x2,y2,x3,y3):
+    
+    vec1=[x2-x1,y2-y1]
+    vec2=[x3-x2,y3-y2]
+    
+    if vec1[0]*vec2[1]-vec1[1]*vec2[0]>0:
+        return 1
+    else:
+        return -1
+if check_line(x1,y1,x2,y2,x3,y3)*check_line(x1,y1,x2,y2,x4,y4)==-1 and check_line(x3,y3,x4,y4,x1,y1)*check_line(x3,y3,x4,y4,x2,y2)==-1:
+    print(1)
+else:
+    print(0)
+
