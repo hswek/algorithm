8225c2cdac14482c8998cebd6a38a4c6fe9b6b77
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:27:03 +0900

    [Bronze IV] Title: 줄번호, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.py
@@ -0,0 +1,10 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+n=int(input())
+for i in range(1,n+1):
+    l=input()
+    print(str(i)+'. '+l)
+    
