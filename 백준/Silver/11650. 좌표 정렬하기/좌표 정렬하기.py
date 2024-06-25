0d4352ebd4130c59858583a08a50c815874d2229
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:55:49 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 520 ms, Memory: 259264 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+import heapq
+from itertools import *
+import math
+sys.setrecursionlimit(10**5)
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(list(map(int,sys.stdin.readline().rstrip().split())))
+
+arr.sort()
+#arr=list(map(int,sys.stdin.readline().rstrip().split()))
+#arr.sort(reverse=True)
+for i in arr:
+    print(i[0],i[1])
