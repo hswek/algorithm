85bfed2b60940bfd6eff376764f54542941befcd
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:23:54 +0900

    [Bronze IV] Title: R2, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.py
@@ -0,0 +1,7 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+n,m=map(int,sys.stdin.readline().rstrip().split())
+print(m+m-n)
