16c02210a5c8a89a10f2defa9ed14766a1133f0d
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:33:39 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
@@ -0,0 +1,10 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+while True:
+    a,b=map(int,sys.stdin.readline().rstrip().split())
+    if a==0 and b==0:
+        break
+    print(a+b)
