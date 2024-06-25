8183b9eeca926a9cda0cf7a6f392c3c33824d23b
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:47:23 +0900

    [Silver IV] Title: 덱, Time: 196 ms, Memory: 117460 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,43 @@
+from collections import deque
+import sys
+n=int(input())
+dq=deque()
+print_list=[]
+for _ in range(n):
+    pp=sys.stdin.readline().rstrip().split()
+    p=pp[0]
+    if p=="push_back":
+        a=(pp[1])
+        dq.append(a)
+    if p=="push_front":
+        a=pp[1]
+        dq.appendleft(a)
+    if p=="pop_front":
+        if len(dq)==0:
+            print_list.append(-1)
+        else:
+            print_list.append(dq.popleft())
+    if p=="pop_back":
+        if len(dq)==0:
+            print_list.append(-1)
+        else:
+            print_list.append(dq.pop())
+    if p=="front":
+        if len(dq)==0:
+            print_list.append(-1)
+        else:
+            print_list.append(dq[0])
+    if p=="back":
+        if len(dq)==0:
+            print_list.append(-1)
+        else:
+            print_list.append(dq[-1])
+    if p=="size":
+        print_list.append(len(dq))
+    if p=="empty":
+        if(len(dq))==0:
+            print_list.append(1)
+        else:
+            print_list.append(0)
+for i in range(len(print_list)):
+    print(print_list[i])
