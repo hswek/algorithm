08daf48b666bcd67c33a3524f7112d1549b5ea85
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:59:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 114500 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,21 @@
+import sys
+n=int(input())
+arr=[-1]*1001
+arr[0]=0
+arr[1]=1
+arr[2]=3
+def a(n):
+    if n==2:
+        return 3
+    if n==1:
+        return 1
+    if arr[n]!=-1:
+        return arr[n]
+    arr[n]=a(n-1)+a(n-2)*2
+    return arr[n]
+a(100)
+a(300)
+a(500)
+a(800)
+a(n)
+print(arr[n]%10007)
