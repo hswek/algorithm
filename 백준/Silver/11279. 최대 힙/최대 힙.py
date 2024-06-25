04977b442231531f0d120f4cb58e49369c2973fb
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:53:26 +0900

    [Silver II] Title: 최대 힙, Time: 224 ms, Memory: 119148 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+n=int(input())
+arr=[]
+for i in range(n):
+    a=int(sys.stdin.readline().rstrip())
+    if a!=0:
+        heapq.heappush(arr,-a)
+    else:
+        if len(arr)==0:
+            print(0)
+        else:
+            answer=heapq.heappop(arr)
+            print(-answer)
+    
