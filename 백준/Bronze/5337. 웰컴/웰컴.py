81e986cd810c87e1cd513b30cf96ce61426526b5
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:29:26 +0900

    [Bronze V] Title: 웰컴, Time: 144 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.py
@@ -0,0 +1,7 @@
+import sys
+from collections import deque
+import heapq
+#a,b=map(int,sys.stdin.readline().rstrip().split())
+print('.  .   .')
+print('|  | _ | _. _ ._ _  _')
+print('|/\\|(/.|(_.(_)[ | )(/.')
