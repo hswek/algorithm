e8b43d19be2e1700631bb79e83c7b36d9d4fc701
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:20:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1412 ms, Memory: 722824 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**5)
+n,m,start=map(int,sys.stdin.readline().rstrip().split())
+arr=[[] for i in range(n+1)]
+for i in range(m):
+    i,j=map(int,sys.stdin.readline().rstrip().split())
+    arr[i].append(j)
+    arr[j].append(i)
+for i in range(1,n+1):
+    arr[i].sort()
+visit=[0]*(n+1)
+num=1
+def dfs(start):
+    global num
+    visit[start]=num
+    num+=1
+    for i in arr[start]:
+        if visit[i]!=0:
+            continue
+        else:
+            dfs(i)
+dfs(start)
+for i in range(1,n+1):
+    print(visit[i])
