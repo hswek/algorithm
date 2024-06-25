0cf5da0cb1925d224d41c109870c6fdf65104bb3
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:09:00 +0900

    [Bronze V] Title: 연세대학교, Time: 144 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.py
@@ -0,0 +1,9 @@
+import sys
+from collections import deque
+import heapq
+#n,m,e=map(int,sys.stdin.readline().rstrip().split())
+n=int(input())
+if n==0:
+    print('YONSEI')
+else:
+    print('Leading the Way to the Future')
