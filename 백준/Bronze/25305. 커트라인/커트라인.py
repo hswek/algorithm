2c2a36a6f56b2191f66a445bf8808c11040338e4
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:22:14 +0900

    [Bronze II] Title: 커트라인, Time: 148 ms, Memory: 242440 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -0,0 +1,10 @@
+import sys
+from collections import deque
+import heapq
+from itertools import *
+import math
+sys.setrecursionlimit(10**5)
+n,m=map(int,sys.stdin.readline().rstrip().split())
+arr=list(map(int,sys.stdin.readline().rstrip().split()))
+arr.sort(reverse=True)
+print(arr[m-1])
