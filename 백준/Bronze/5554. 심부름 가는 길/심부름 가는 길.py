586cac0ef8bc6ab1a77971de838d282181ec27c8
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:32:04 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.py
@@ -0,0 +1,12 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+a=a+b+c+d
+print(a//60)
+print(a%60)
