2fa530a49f68c976fcb01b0736ed389430abed25
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:29:42 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 132 ms, Memory: 114432 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.py
@@ -0,0 +1,9 @@
+import sys
+from collections import deque
+import heapq
+#a,b=map(int,sys.stdin.readline().rstrip().split())
+print('       _.-;;-._')
+print('\'-..-\'|   ||   |')
+print('\'-..-\'|_.-;;-._|')
+print('\'-..-\'|   ||   |')
+print('\'-..-\'|_.-\'\'-._|')
