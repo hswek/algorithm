bfca028c35cbe42eac78eff4fed3906a3218fb7e
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:08:49 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.py
@@ -0,0 +1,8 @@
+import sys
+#from collections import deque
+#import math
+#import heapq
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+n=int(input())
+for i in range(n,0,-1):
+    print('*'*i)
