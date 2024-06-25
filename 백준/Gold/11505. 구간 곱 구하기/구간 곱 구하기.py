5f146368736baf52cca50303b6c52738f659291b
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:55:33 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 764 ms, Memory: 341732 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -0,0 +1,55 @@
+import sys
+from collections import deque
+import heapq
+import math
+sys.setrecursionlimit(int(10**5))
+#t=int(sys.stdin.readline().rstrip())
+#n,k=map(int,sys.stdin.readline().rstrip().split())
+#arr=[-1]+list(map(int,sys.stdin.readline().rstrip().split()))
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+
+def merge(left,right):
+    return left*right%1000000007
+def build(left,right,idx):
+    if left==right:
+        seg_tree[idx]=arr[left]
+        return seg_tree[idx]
+    mid=(left+right)//2
+    
+    left_val=build(left,mid,idx*2)
+    right_val=build(mid+1,right,idx*2+1)
+    seg_tree[idx]=merge(left_val,right_val)
+    return seg_tree[idx]
+def query(left,right,node_left,node_right,idx):
+    if right<node_left or left>node_right:
+        return 1
+    if right>=node_right and left<=node_left:
+        return seg_tree[idx]
+    node_mid=(node_left+node_right)//2
+    left_val=query(left,right,node_left,node_mid,idx*2)
+    right_val=query(left,right,node_mid+1,node_right,idx*2+1)
+    return merge(left_val,right_val)
+
+def update(left,right,idx,want_to_idx,want_to_val):
+    if want_to_idx< left or want_to_idx> right:
+        return seg_tree[idx]
+    if left==right:
+        seg_tree[idx]=want_to_val
+        return want_to_val
+    mid=(left+right)//2
+    left_val=update(left,mid,idx*2,want_to_idx,want_to_val)
+    right_val=update(mid+1,right,idx*2+1,want_to_idx,want_to_val)
+    seg_tree[idx]=merge(left_val,right_val)
+    return seg_tree[idx]
+n,m,k=map(int,sys.stdin.readline().rstrip().split())
+arr=[]
+seg_tree=[0]*(n*4)
+for _ in range(n):
+    arr.append(int(sys.stdin.readline().rstrip()))
+build(0,len(arr)-1,1)
+for _ in range(m+k):
+    a,b,c=map(int,sys.stdin.readline().rstrip().split())
+    if a==1:
+        update(0,n-1,1,b-1,c)
+    if a==2:
+        print(query(b-1,c-1,0,n-1,1))
