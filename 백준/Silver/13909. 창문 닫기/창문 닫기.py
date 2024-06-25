f0c5cb37c9fdee00426e65f34a18ae81c26b76c0
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 20:03:29 +0900

    [Silver V] Title: 창문 닫기, Time: 144 ms, Memory: 242440 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+import heapq
+from itertools import *
+import math
+sys.setrecursionlimit(10**5)
+#n,m=map(int,sys.stdin.readline().rstrip().split())
+#arr=list(map(int,sys.stdin.readline().rstrip().split()))
+n=int(input())
+print(int(math.sqrt(n)))
+    
