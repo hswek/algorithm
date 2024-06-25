b75818642b68eeedaf3a9c114bda46d4ce5f9c85
hswek <70687398+hswek@users.noreply.github.com>
2024-06-25 19:34:11 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 440 ms, Memory: 144052 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,47 @@
+import sys
+arr=list(map(int,sys.stdin.readline().rstrip().split()))
+
+def get_include_middle(start,mid,end):
+    height=min([arr[mid],arr[mid-1]])
+    i=mid-2
+    j=mid+1
+    length=2
+    area=height*length
+    while(i>=start and j<end):
+        #print(i,start,j,end)
+        if arr[i]>arr[j]:
+            height=min(height,arr[i])
+            i-=1
+            length+=1
+            area=max(area,length*height)
+        else:
+            height=min(height,arr[j])
+            j+=1
+            length+=1
+            area=max(area,length*height)
+    while(i>=start):
+        height=min(height,arr[i])
+        i-=1
+        length+=1
+        area=max(area,length*height)
+    while(j<end):
+        height=min(height,arr[j])
+        j+=1
+        length+=1
+        area=max(area,length*height)
+    return area
+
+def answer(start,end):
+    if start+1==end:
+        return arr[start]
+    mid=(start+end)//2
+    first=answer(start,mid)
+    two=answer(mid,end)
+    three=get_include_middle(start,mid,end)
+    return max([first,two,three])
+
+while(arr!=[0]):
+    arr.pop(0)
+    #print(arr)
+    print(answer(0,len(arr)))
+    arr=list(map(int,sys.stdin.readline().rstrip().split()))
