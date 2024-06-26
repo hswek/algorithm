8e2b980053f0c4c1ec3aad541730465b1a4b065c
hswek <70687398+hswek@users.noreply.github.com>
2024-06-26 19:09:58 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.83 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = []
+    d={}
+    for i in range(len(s)):
+        if s[i] not in d:
+            d[s[i]]=i
+            answer.append(-1)
+        else:
+            answer.append(i-d[s[i]])
+            d[s[i]]=i
+    return answer
